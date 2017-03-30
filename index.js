e641a1fcf37103768c2a4af7f27e45365b195d22
Prathap Reddy <PrathapaReddy.CC@cognizant.com>
2017-03-30 10:08:52 +0530

    redirect all requests to https://

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
+// Redirect the request to HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
